40aee80c0da7b5321e73eaeb3dbf85c93e5cff73
Kirk Reynolds <kreynolds6805@gmail.com>
2022-12-19 20:12:18 -0800

    Final Commit

---
+++ profile.js
@@ -0,0 +1,24 @@
+const colorButton = document.getElementById('color');
+
+colorButton.addEventListener('click', () => {
+    alert('My fav color is green.');
+
+});
+
+
+
+
+const favPlaceButton = document.getElementById('place');
+
+favPlaceButton.addEventListener('click', () => {
+    alert('My fav place is the lake.');
+
+});
+
+
+const favoriteRitual = document.getElementById('ritual');
+
+favoriteRitual.addEventListener('click', () => {
+    alert('My fav ritual is learning.');
+
+});
